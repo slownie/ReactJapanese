3683fa3ced3a4ff7d1a6206e8a3c3fb92d029aa8
Samuel <slownie16@gmail.com>
2020-04-22 10:47:33 -0400

    a ton of stuff

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { SafeAreaView, Text, Button, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+{/*Navigation Screens */}
+function HomeScreen() {
+    return (
+        <SafeAreaView style = {{ flex: 1, alignItems: 'center', justifyContent: 'center'}}> 
+            <StatusBar barStyle = 'light-content' backgroundColor = '#00675b' />
+            <Text>Idiot</Text>
+        </SafeAreaView>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+{/*Stack Navigation */}
+function HomeStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name = '日本語'
+                component = {HomeScreen}
+                options = {{
+                    headerTitleAlign: 'center',
+                    headerTintColor: '#fff',
+                    headerTitleStyle: { fontWeight: 'bold' },
+                    headerStyle: { backgroundColor: '#009688' },
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
+
+{/*Main Function */}
+function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+            
+            >
+                <Tab.Screen name = "Home" component = {HomeStack} />
+                <Tab.Screen name = "Basics" component = {HomeStack} />
+                <Tab.Screen name = "Grammar" component = {HomeStack} />
+                <Tab.Screen name = "Vocabulary" component = {HomeStack} />
+            
+            </Tab.Navigator>
+        </NavigationContainer>    
+    );
+}
+
+export default App;
